eb3f92b141bc401303f725753b115f666a377918
Marius Stoica <Letto2@gmail.com>
2019-01-16 22:22:42 +0200

    Split to separate operation function

--- calculator.cpp
+++ calculator.cpp
@@ -43,6 +43,7 @@
 #include <cmath>
 
 using std::cout;
+using std::cerr;
 using std::endl;
 using std::cin;
 
@@ -53,182 +54,201 @@ using std::istream_iterator;
 using std::ostream_iterator;
 
 
-int main(int argc,char* argv[]) {
-    
+double do_operation( string operation) {
     while(true) {
-	string operation;
-	vector<string> words;
-    
-        cout << "Enter operation: " << endl;
-	std::getline( cin, operation);
-	if( tolower(operation[0]) == 'x' || tolower(operation[0]) == 'q') {
-	    exit(EXIT_SUCCESS);
-	}
+        vector<string> words;
 
+        std::istringstream iss( operation);
+        char op;
+        {
+            iss >> operation;
 
-	// remove trailing white space
-	while( !operation.empty() && std::isspace( operation.back())) {
-	    operation.pop_back();
-	}
-
-	bool valid = true;
-	std::istringstream iss( operation);
-	char op;
-	{
-	    iss >> operation;
-	    
             if( operation.length() > 2) {
-		    cout << "Unrecognized operation: " << operation << endl;
-		    valid = false;
+                cerr << "Unrecognized operation: " << operation <<"\n";
+                throw "Unrecognized operation: " + operation + "\n";
             }
 
-	    op = operation[0];
-	    if( operation.length() == 2) {
-		switch( op) {
-		case '*':
-		    if( operation[1] == '*') {
-			op = 'p';
-		    }
-		    break;
-		case '>':
-		    if( operation[1] == '>') {
-			op = 'r';
-		    }
-		    break;
-		case '<':
-		    if( operation[1] == '<') {
-			op = 'l';
-		    }
-		    break;
-		default:
-		    cout << "Warning: code should never be reached!" << endl;
-		}
-		// cout << "Operation is: " << operation << endl << "Op is: " << op << endl;
-	    }
-	}
+            op = operation[0];
+            if( operation.length() == 2) {
+                switch( op) {
+                    case '*':
+                        if( operation[1] == '*') {
+                            op = 'p';
+                        } else {
+			    // cerr << "Operation: " << operation << endl;
+			    throw "Unrecognized operation: ";
+			}
+                        break;
+                    case '>':
+                        if( operation[1] == '>') {
+                            op = 'r';
+                        }
+                        break;
+                    case '<':
+                        if( operation[1] == '<') {
+                            op = 'l';
+                        }
+                        break;
+                    default:
+                        cerr << "Warning: code should never be reached!" << endl;
+                }
+                // cout << "Operation is: " << operation << endl << "Op is: " << op << endl;
+            }
+        }
 
-	vector<double> elements;
-	while( iss) {
-	    string out;
-	    iss >> out;
-	    if( out.find_first_not_of(".-+0123456789") != string::npos) {
-		cout << "Element " << out << " is not a number." << endl;
-		valid = false;
-	    }
-	    if(iss) {
-		elements.push_back( strtod(out.c_str(), nullptr));
-		/* cout << "Element " << strtod(out.c_str(), nullptr) 
-		     << " added to the elements vector" << endl; */
-	    }
-	}
+        vector<double> elements;
+        while( iss) {
+            string out;
+            iss >> out;
+            if( out.find_first_not_of("().-+0123456789") != string::npos) {
+                // TODO: crashes when encountered
+                throw "Element " + out + " is not a number.\n";
+            }
+            if(iss) {
+                elements.push_back( strtod(out.c_str(), nullptr));
+                /* cout << "Element " << strtod(out.c_str(), nullptr)
+                 *  << " added to the elements vector" << endl; */
+            }
+        }
 
-	// Display the elements vector.
-	// cout << "Vector contains the elements: " << endl;
-	// std::copy( elements.begin(), elements.end(), std::ostream_iterator<double>(cout, " "));
-	// cout << endl;
+        // Display the elements vector.
+        // cout << "Vector contains the elements: " << endl;
+        // std::copy( elements.begin(), elements.end(), std::ostream_iterator<double>(cout, " "));
+        // cout << endl;
 
-	double res;
+        double res;
         switch (op) {
-	    case 'x':
-	    case 'X':
-	    case 'q':
-	    case 'Q':
-	    case '\0':
-		exit(EXIT_SUCCESS);
-		break;
-	    case '+':
-		res = 0;
-		for( auto& n: elements) {
-		    res += n;
-		}
-	        break;
-	    case '-': {
-		res = elements[0];
-		for( int i = 1; i < elements.size(); ++i) {
-		    res -= elements[i];
-		}
-	    }
-		break;
-	    case '*':
-		res = 1;
-		for( auto& n: elements) {
-		    res *= n;
-		}
-		break;
-	    case '/':
-		res = elements[0];
-		for( int i = 1; i < elements.size(); ++i) {
-		    if( elements[i] != 0) {
-			    res /= elements[i];
-		    } else {
-			cout << "Error: Division by 0." << endl;
-			valid = false;
-			continue;
-		    }
-		}
-		break;
-	    case 'p':
-		res = elements[0];
-		for( int i = 1; i < elements.size(); ++i) {
-		    res = pow( res, elements[i]);
-		}
-		break;
-	    case '%': {
+            case 'x':
+            case 'X':
+            case 'q':
+            case 'Q':
+            case '\0':
+                exit(EXIT_SUCCESS);
+                break;
+            case '+':
+                res = 0;
+                for( auto& n: elements) {
+                    res += n;
+                }
+                break;
+            case '-': {
+                res = elements[0];
+                for( int i = 1; i < elements.size(); ++i) {
+                    res -= elements[i];
+                }
+            }
+            break;
+            case '*':
+                res = 1;
+                for( auto& n: elements) {
+                    res *= n;
+                }
+                break;
+            case '/':
+                res = elements[0];
+                for( int i = 1; i < elements.size(); ++i) {
+                    if( elements[i] != 0) {
+                        res /= elements[i];
+                    } else {
+                        throw "Error: Division by 0.";
+                    }
+                }
+                break;
+            case 'p':
+                res = elements[0];
+                for( int i = 1; i < elements.size(); ++i) {
+                    res = pow( res, elements[i]);
+                }
+                break;
+            case '%': {
                 long x = elements[0];
                 for( int i = 1; i < elements.size(); ++i) {
                     x %= static_cast<long>( elements[i]);
                 }
-		res = x;
-	    }
-		break;
-	    case '^':{
-		long x = elements[0];
+                res = x;
+            }
+            break;
+            case '^':{
+                long x = elements[0];
                 for( int i = 1; i < elements.size(); ++i) {
-		    x ^= static_cast<long>( elements[i]);
+                    x ^= static_cast<long>( elements[i]);
                 }
-		res = x;
+                res = x;
+            }
+            break;
+            case '|': {
+                long x = elements[0];
+                for( int i = 1; i < elements.size(); ++i) {
+                    x |= static_cast<long>( elements[i]);
+                }
+                res = x;
+            }
+            break;
+            case '&': {
+                long x = elements[0];
+                for( int i = 1; i < elements.size(); ++i) {
+                    x &= static_cast<long>( elements[i]);
+                }
+                res = x;
+            }
+            break;
+            case 'l': {
+                long x = elements[0];
+                for( int i = 1; i < elements.size(); ++i) {
+                    x <<= static_cast<long>( elements[i]);
+                }
+                res = x;
+            }
+            break;
+            case 'r': {
+                long x = elements[0];
+                for( int i = 1; i < elements.size(); ++i) {
+                    x >>= static_cast<long>( elements[i]);
+                }
+                res = x;
+            }
+            break;
+            case '(':
+                return do_operation( operation);
+            break;
+            case ')':
+                return res;
+            break;
+            default:
+		// cerr << "Switch( op) default reached." << endl;
+                throw "Unrecognized operation: " + op;
+        }
+        return res;
+    }
+}
+
+
+int main(int argc,char* argv[]) {
+    string operation;
+    double res;
+
+    while( true) {
+        try {
+            cout << "Enter operation: " << endl;
+            std::getline( cin, operation);
+            if( tolower(operation[0]) == 'x' || tolower(operation[0]) == 'q') {
+                exit(EXIT_SUCCESS);
+            }
+
+            // remove trailing white space
+            while( !operation.empty() && std::isspace( operation.back())) {
+                operation.pop_back();
+            }
+
+            res = do_operation( operation);
+        } catch( const char* msg) {
+	    if( msg) {
+		cerr << "Error message: " << msg << endl;
+		continue;
 	    }
-		break;
-	    case '|': {
-		long x = elements[0];
-		for( int i = 1; i < elements.size(); ++i) {
-		    x |= static_cast<long>( elements[i]);
-		}
-		res = x;
-	    }
-		break;
-	    case '&': {
-		long x = elements[0];
-		for( int i = 1; i < elements.size(); ++i) {
-		    x &= static_cast<long>( elements[i]);
-		}
-		res = x;
-	    }
-		break;
-	    case 'l': {
-		long x = elements[0];
-		for( int i = 1; i < elements.size(); ++i) {
-		    x <<= static_cast<long>( elements[i]);
-		}
-		res = x;
-	    }
-		break;
-	    case 'r': {
-		long x = elements[0];
-		for( int i = 1; i < elements.size(); ++i) {
-		    x >>= static_cast<long>( elements[i]);
-		}
-		res = x;
-	    }
-		break;
-	    default:
-		cout << "Unrecognized operation: " << operation << endl;
-		exit(2);
-	}
-	if( valid) {
-	    cout << "Result: " << res << endl;
-	}
-    } 
+        }
+        cout << "Result: " << res << endl;
+    }
     return EXIT_SUCCESS;
 }
 
